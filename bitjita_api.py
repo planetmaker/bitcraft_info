31bbc8350603a173621141ec4fa9143b543828e1
planetmaker <bitcraft@planetmaker.de>
2025-12-04 22:09:37 +0100

    Change [api]: Player storage log

--- bitjita_api.py
+++ bitjita_api.py
@@ -17,6 +17,9 @@ def get_url_player_inventories(playerID: int):
 def get_url_storage_logs(buildingID: int):
     return("https://bitjita.com/api/logs/storage?buildingEntityId={}".format(buildingID))
 
+def get_url_player_logs(playerID: int, limit=1000, since="2025-11-01T00:00:00Z"):
+    return("https://bitjita.com/api/logs/storage?playerEntityId={}&limit={}&since={}".format(playerID, limit, since))
+
 def get_url_items_info(search_str = None):
     url = "https://bitjita.com/api/items"
     if search_str is not None:
@@ -91,6 +94,10 @@ def get_items_info(search_str = None):
         ret_dict[key].pop("id")
     return(ret_dict)
 
+def get_item_name(itemID: int):
+    string = ""
+    return string
+
 def get_cargos_info(search_str = None):
     url = get_url_cargos_info(search_str)
 
@@ -103,6 +110,31 @@ def get_cargos_info(search_str = None):
         ret_dict[key] = entry
         ret_dict[key].pop("id")
     return(ret_dict)
+
+def get_player_logs(playerID:int, claimID: int, limit=1000, since="2025-12-01T00:00:00Z"):
+    per_storage = {}
+    summary = {}
+    url = get_url_player_logs(playerID, limit=limit, since=since)
+    raw_logs = read_url_json(url)
+    for entry in raw_logs.get("logs"):
+        quantity = entry.get("data").get("quantity")
+        quantity = quantity * (1 if entry.get("data").get("type") == "deposit_item" else -1)
+        itemID   = entry.get("data").get("item_id")
+        storageID = entry.get("objectEntityId")
+
+        if storageID not in per_storage:
+            per_storage[storageID] = {}
+        if itemID not in per_storage.get(storageID):
+            per_storage[storageID][itemID] = quantity
+        else:
+            per_storage[storageID][itemID] += quantity
+
+        if itemID not in summary:
+            summary[itemID] = quantity
+        else:
+            summary[itemID] += quantity
+
+    return (per_storage, summary)
 
 def get_player_itemtype(itemtypeID):
     if itemtypeID == 0:
