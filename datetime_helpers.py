16cd94d6f35a19bb3d4fb2bd33f1a6fc73fb6c28
planetmaker <bitcraft@planetmaker.de>
2026-05-16 12:02:27 +0200

    Fix [datetime_helpers]: Use the correct formating for the time string as expected by bitjita when asking for logs

--- datetime_helpers.py
+++ datetime_helpers.py
@@ -11,9 +11,9 @@ import datetime
 def time_string(time = None):
     if time is None:
         time = datetime.datetime.now()
-    return(time.strftime("%d.%m.%Y %H:%Mh"))
+    return(time.strftime("%Y-%m-%d %H:%M"))
 
 def time_string_offset(time = None, offset_hours = 0):
     if time is None:
-        time = datetime.datetime.now() + datetime.timedelta(days=offset_hours)
+        time = datetime.datetime.now() + datetime.timedelta(hours=offset_hours)
     return(time_string(time))
