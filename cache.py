822b5773df14fed93249c2b52a9c96dfe39ffa51
planetmaker <bitcraft@planetmaker.de>
2026-05-17 07:43:10 +0200

    Change [cache]: Allow to specify the variables to cache via parameter

--- cache.py
+++ cache.py
@@ -13,13 +13,17 @@ from config import config
 cache_path = 'cache/'
 filename_items_table = cache_path + 'items_cache.csv'
 
-def write_cache():
-    if 'items' in config.get('cache'):
+def write_cache(what = None):
+    if what is None:
+        what = config.get('cache')
+    if 'items' in what:
         global items_table
         items_table.to_csv(filename_items_cache)
 
-def read_cache():
-    if 'items' in config.get('cache'):
+def read_cache(what = None):
+    if what is None:
+        what = config.get('cache')
+    if 'items' in what:
         global items_table
         items_table = pd.read_csv(filename_items_cache)
 
