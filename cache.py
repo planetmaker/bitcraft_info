54015d5a4b6fcb3a294ce060ca9a56f6dd49a0ed
planetmaker <bitcraft@planetmaker.de>
2026-05-28 12:21:00 +0200

    Add [cache]: Create cache directory, if it does not exist

--- cache.py
+++ cache.py
@@ -10,13 +10,21 @@ import pandas as pd
 # from helpers import items_table, claim_members_table, claim_buildings, claim_inventory, player_houses_inventory, player_deployables_inventory
 from variables import *
 from config import config
+# import os
+from pathlib import Path
 
 cache_path = 'cache/'
 filename_items_cache = cache_path + 'items_cache.csv'
 
+def create_cache_path():
+#    if not os.path.isdir(cache_path):
+    Path(cache_path).mkdir(parents=True, exist_ok=True)
+
+
 def write_cache(what = None):
     if what is None:
         what = config.get('cache')
+    create_cache_path()
     if 'items' in what:
         print("Writing items table to {}".format(filename_items_cache))
         global items_table
